d38c9ad8403b3e4c5d3279a864db453e3ed3e38a
githinjisamson1 <githinjisamson148@gmail.com>
2023-10-03 02:15:45 +0300

    Compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+// fetch API
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: name, email: email }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      // console.log(data);
+      document.querySelector("body").textContent = data.id;
+    })
+    .catch((err) => {
+      // console.log(err);
+      document.querySelector("body").textContent = err;
+    });
+}
+
+// submitData("John Doe", "johndoe@gmail.com");
